0235f20f50e880dc0211f10714b25f09bcf77f2c
Rafael Sanz Prades <rafaelsanzprades@gmail.com>
2026-07-18 23:44:22 +0200

    chore: massive cleanup of unused files and migration scripts

--- frontend/src/services/fix_mobiles.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'c:/GD-rsp/APP/frontend/src/services/demo-ele203-0237ictve-curso202526.ts';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/"Movil":\s*"6/g, '"Movil": "5');
-fs.writeFileSync(file, content);
-console.log('Done');
